78b2b0b6ceb4833177fa3b66937f0ae7688cd20f
Romain Alexandre <romain.alexandre@intellectionsoftware.com>
2018-11-09 07:54:15 +0000

    Add dungeon generator example

---
+++ tests_games/dungeon_generator/js/index.js
@@ -0,0 +1,20 @@
+import Phaser from "phaser";
+import DungeonScene from "./dungeon_scene.js";
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: "#000",
+  parent: "game-container",
+  pixelArt: true,
+  scene: DungeonScene,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 }
+    }
+  }
+};
+
+const game = new Phaser.Game(config);
